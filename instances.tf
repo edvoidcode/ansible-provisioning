d9e461513a34e35208b8208f0dbce463a78a44fb
Edson  Silva <edson.silva@findmore.pt>
2021-10-25 22:20:04 +0100

    adding instance

--- instances.tf
+++ instances.tf
@@ -1,6 +1,16 @@
 
 resource "aws_instance" "node-wp" {
-   
+   ami =
+   instance_type               = var.ami-id
+   subnet_id                   =
+   vpc_security_group_ids      =
+   associate_public_ip_address =
+   user_data                   =
+   key_name                    =
+   tags = {
+     Name = "lab-wp"
+   }
+   // depends_on = [
 
    provisioner "local-exec" {
       command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u {var.user} -i '${self.ipv4_address},' 
--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "cidr_block" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+variable "ami-id" {
+  type        = string
+  default     = "ami-0a8e758f5e873d1c1"
+  description = "AWS Image Ubuntu Server 20.04 LTS (HVM)"
+}
