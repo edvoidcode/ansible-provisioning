545b8c0f5fc029edd094774413f47d4601b4e9a1
Edson  Silva <edson.silva@findmore.pt>
2021-10-25 16:28:45 +0100

    adding pipeline

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,17 @@
+<<<<<<< HEAD
+=======
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+>>>>>>> 23b616ca9b5ef3f5aa356ab82ce2740348866f1a
 resource "aws_vpc" "lab-vpc" {
    cidr_block = var.cidr_block
    instance_tenancy = "default"
@@ -6,4 +20,12 @@ resource "aws_vpc" "lab-vpc" {
       Name = "lab-vpc"
       Owner = "edson"
    }
+<<<<<<< HEAD
+=======
+}
+
+resource "aws_subnet" "sub-pb"{
+   vpc_id = aws_vpc.lab-vpc.id 
+
+>>>>>>> 23b616ca9b5ef3f5aa356ab82ce2740348866f1a
 }
